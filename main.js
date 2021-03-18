33f760caacf188ad61298d794fb77f3c2d5d04ce
Welington Chanda <iamwchanda@gmail.com>
2021-03-18 10:27:43 +0200

    connect_4

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // TODO : construct grid
+
+    const connect4 = new Connect4('#connect4')
+
+    connect4.onPlayerMove = function() {
+        $('#player').text(connect4.player);
+    }
+
+
+    $('#restart').click(function() {
+        connect4.restart()
+    })
+})
